27a0f2bd39299a1e76b8975a4795858ca94d6c8d
jonlk <jlkovach01@gmail.com>
2023-03-04 12:29:52 -0500

    added routes

--- web/controller.go
+++ web/controller.go
@@ -1,56 +1,10 @@
 package web
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-	"strings"
-
 	"github.com/gin-gonic/gin"
-	"github.com/jonlk/atmosphere-v2/atmosphere"
 )
 
 func RegisterRoutes(group *gin.RouterGroup) {
-
-	group.GET("/dewpoint", func(ctx *gin.Context) {
-		var errors []string
-
-		temperatureInCelsius, err := strconv.ParseFloat(ctx.Query("temperatureInCelsius"), 64)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-
-		relativeHumidityPercentage, err := strconv.ParseFloat(ctx.Query("relativeHumidityPercentage"), 64)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-
-		if len(errors) > 0 {
-			ctx.String(http.StatusBadRequest, strings.Join(errors, " | "))
-		} else {
-			result := atmosphere.CalculateDewPoint(temperatureInCelsius, relativeHumidityPercentage)
-			ctx.String(http.StatusOK, fmt.Sprintf("%.2f", result))
-		}
-	})
-
-	group.GET("/relativehumidity", func(ctx *gin.Context) {
-		var errors []string
-
-		temperatureInCelsius, err := strconv.ParseFloat(ctx.Query("temperatureInCelsius"), 64)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-
-		dewpointInCelsius, err := strconv.ParseFloat(ctx.Query("dewpointInCelsius"), 64)
-		if err != nil {
-			errors = append(errors, err.Error())
-		}
-
-		if len(errors) > 0 {
-			ctx.String(http.StatusBadRequest, strings.Join(errors, " | "))
-		} else {
-			result := atmosphere.CalculateRelativeHumidity(temperatureInCelsius, dewpointInCelsius)
-			ctx.String(http.StatusOK, fmt.Sprintf("%.2f", result))
-		}
-	})
+	group.GET("/dewpoint", dewpointRoute)
+	group.GET("/relativehumidity", relativeHumidityRoute)
 }
